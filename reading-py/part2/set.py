1c1e2729fa2bc8d28ceb3edcbce5b94d9e2a6aac
KALU56 <kalkidanbehailu55@gmail.com>
2025-01-16 14:38:22 +0300

    some ex

--- reading-py/part2/set.py
+++ reading-py/part2/set.py
@@ -15,3 +15,20 @@ print(languges)
 animals={"cat","dog","fish","rabbit","elephant"}
 "cat" in animals
 print(animals)
+#Q6. Check if "cat" is in the set {"dog", "cat", "rabbit"} .
+pets={"dog","cat","rabbit"}
+"cat" in pets
+print(pets)
+##Medium Exercises
+#Q1. Combine the sets {1, 2, 3} and {3, 4, 5} using union() .
+num1={1,2,3}
+num2={3,4,5}
+num3=num1.union(num2)
+print(num3)
+#Q2. Find the intersection of the sets {1, 2, 3} and {3, 4, 5} using intersection() .
+num4=num1.intersection(num2)
+print(num4)
+#Q3. Find the difference between the sets {1, 2, 3} and {3, 4, 5} using difference() .
+num5=num1.difference(num2)
+print(num5)
+
